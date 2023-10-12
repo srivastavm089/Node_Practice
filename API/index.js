8bca181d7559d03e8b8e2837e85f1da9357a6e1c
Abhay Kumar Srivastava <ms2364911@gmail.com>
2023-10-12 14:48:06 +0530

    how to get files data using post added

--- API/index.js
+++ API/index.js
@@ -1,35 +1,43 @@
-const express = require('express');
-require('./config');
-const Product = require('./student');
-
+const express = require("express");
+require("./config");
+const Product = require("./student");
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.post("/create", async (req,res)=>{
-    let data = new Product(req.body);
-    let result = await data.save();
-    console.log(result)
-    res.send("Done")
+app.post("/create", async (req, res) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(result);
+  res.send("Done");
 });
-app.get("/list", async(req,res)=>{
-    let data = await Product.find();
-    res.send(data);
+app.get("/list", async (req, res) => {
+  let data = await Product.find();
+  res.send(data);
+});
+app.delete("/delete/:_id", async (req, res) => {
+  let data = await Product.deleteOne(req.params);
 
-})
-app.delete('/delete/:_id', async(req,res)=>{
-    let data = await Product.deleteOne(req.params)
-   
-    res.send(data)
-})
-app.put("/update/:_id", async (req,res)=>{
-   
-    let data = await Product.updateOne(
-        req.params,
-        {$set:req.body}
-    )
-    res.send(data);
-})
-app.search
+  res.send(data);
+});
+app.put("/update/:_id", async (req, res) => {
+  let data = await Product.updateOne(req.params, { $set: req.body });
+  res.send(data);
+});
+app.get("/search/:key", async (req, res) => {
+  console.log(req.params.key);
+  let data = await Product.find({
+    "$or": [
+        { "name": { $regex:req.params.key}},
+       
+        
+
+    ]   
+  });
+  console.log(data)
+  res.send("search done");
+});
+
 app.listen(5000);
+
 // 6523adffa029d7d5572f335d
--- API/student.js
+++ API/student.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     name:String,
     age:Number,
+   
 
 });
 
